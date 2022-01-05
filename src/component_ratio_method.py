cc38d853bb01c9f2a9a301e139647dd61865335b
Christopher Mowry <cmowry84@gmail.com>
2022-01-04 23:56:53 -0800

    added logic for volcfgrs

--- src/component_ratio_method.py
+++ src/component_ratio_method.py
@@ -215,14 +215,242 @@ class Component_Ratio_Method(object):
                 self._calcStumpBiomassLbs(species, dbh, height))
 
 
+    def getVOLCFGRS(self, species, region_id, x1, x2, x3, v1, v2):
+
+        # Northeastern States (CT,DE,ME,MD,MA,NH,NJ,NY,OH,PA,RI,VT,WV) Table 1
+        # Table 1 Row 5
+        if region_id == 'S24':
+
+            # TODO implement the calculation here
+            print('Table 1 Row 5')
+
+        # Southern States (AL,AR,FL,GA,KY,LA,MS,NC,OK,SC,TN,TX,VA) Table 2
+        elif region_id == 'S33':
+
+            # Table 2 Row 1
+            if species['species_cd'] not in [58, 59, 69, 106, 140, 
+                                             141, 61, 63, 66, 303,
+                                             321, 755, 756, 758, 810,
+                                             843, 846, 867, 8513, 122, 202]:
+
+                # TODO implement the calculation here
+                print('Table 2 Row 1')
+
+            # Table 2 Row 2
+            elif species['species_cd'] in [58, 59, 69, 106, 140, 141]:
+
+                # TODO implement the calculation here
+                print('Table 2 Row 2')
+
+            # Table 2 Row 3
+            elif species['species_cd'] in [61, 63, 66, 303, 321,
+                                           755, 756, 758, 810, 843, 
+                                           846, 867, 8513]:
+
+                # TODO implement the calculation here
+                print('Table 2 Row 3')
+
+            # Table 2 Row 4
+            elif species['species_cd'] == 122:
+
+                # TODO implement the calculation here
+                print('Table 2 Row 4')
+
+            # Table 2 Row 5
+            elif species['species_cd'] == 202:
+
+                # TODO implement the calculation here
+                print('Table 2 Row 5')
+
+            else:
+                raise Exception("Unknown equation for region and species!")
+
+        # Central States (IL,IN,IA,MO), Lake States (MI,MN,WI), & Plains States (KS,NE,ND,SD) Table 1
+        elif region_id in ['S23LCS',  'S23LLS', 'S23LPS']:
+            
+            # Table 1 Row 1
+            if (( region_id in ['S23LCS', 'S23LPS'] and species['species_cd'] not in [66, 122] ) 
+                    or ( region_id == 'S23LCS' and species['species_cd'] == 122 ) ):
+
+                # TODO implement the calculation here
+                print('Table 1 Row 1')
+
+            # Table 1 Row 2
+            elif region_id == 'S23LLS' and species['species_cd'] != 66:
+                # TODO implement the calculation here
+                print('Table 1 Row 2')
+
+            # Table 1 Row 4
+            elif region_id == 'S23LPS' and species['species_cd'] == 122:
+                # TODO implement the calculation here
+                print('Table 1 Row 4')
+
+            # Table 1 Row 3
+            elif species['species_cd'] == 66:
+                # TODO implement the calculation here
+                print('Table 1 Row 3')
+            else:
+                raise Exception("Unknown equation for region and species!")
+
+        # Rocky Mountain States (AZ, CO, ID, MT, NM, NV, UT, WY) Table 3
+        elif region_id in ['S22LAZN', 'S22LAZS', 
+                           'S22LCOE', 'S22LCOW',
+                           'S22LID',
+                           'S22LMTE', 'S22LMTW',
+                           'S22LNMN', 'S22LNMS', 
+                           'S22LNV',
+                           'S22LUTNE', 'S22LUTSW',
+                           'S22LWYE', 'S22LWYW']:
+
+            # Table 3 Row 1
+            if ((( region_id in ['S22LAZN', 'S22LAZS', 'S22LNMN', 'S22LNMS', 'S22LUTSW'] )
+                   and ( species['species_cd'] in [15, 17, 18, 19, 20, 
+                                               21, 22, 93, 94, 96, 
+                                               101, 102, 104, 108, 113, 
+                                               114, 142, 202, 351, 352, 
+                                               353, 374, 375, 746] )) 
+                  or ( species['species_cd'] == 103 )) :
+
+                # TODO implement the calculation here
+                print('Table 3 Row 1')
+
+            # Table 3 Row 2
+            elif ((( region_id in ['S22LCOE', 'S22LCOW', 'S22LNV', 'S22LUTNE', 'S22LWYE', 'S22LWYW'] )
+                     and ( species['species_cd'] in [15, 17, 18, 19, 20, 
+                                                     21, 22, 93, 94, 96, 
+                                                     101, 102, 104, 108, 113, 
+                                                     114, 142, 202, 351, 352, 
+                                                     353, 374, 375, 746] ))
+
+                    or (( region_id in ['S22LCOE', 'S22LMTE', 'S22LWYE'])
+                          and (species['species_cd'] in [51, 112, 116, 118, 122, 135, 137, 231]))
+                          
+                    or ((region_id in ['S22LCOE', 'S22LCOW', 'S22LNV','S22LWYE', 'S22LWYW'])
+                         and (species['species_cd'] == 104 ))):
+
+                # TODO implement the calculation here
+                print('Table 3 Row 2')
+
+            # Table 3 Row 3
+            elif ((( region_id in ['S22LID', 'S22LMTE', 'S22LMTW'] )
+                     and ( species['species_cd'] in [15, 17, 20, 21, 22, 
+                                                     72, 73, 101, 102, 104, 
+                                                     108, 113, 114, 142, 202] ))
+                                                     
+                    or (( region_id in ['S22LID', 'S22LMTW'])
+                          and ( species['species_cd'] in [51, 112, 116, 118, 122, 
+                                                          135, 137, 231] ))
+                    
+                    or (( region_id in ['S22LAZS', 'S22LCOW','S22LID',
+                                        'S22LMTE', 'S22LMTW', 'S22LNMS',
+                                        'S22LNV', 'S22LWYW'] )
+                          and ( species['species_cd'] in [117, 119] ))):
+
+                # TODO implement the calculation here
+                print('Table 3 Row 3')
+            
+            # Table 3 Row 4
+            elif (((region_id in ['S22LID', 'S22LMTE', 'S22LMTW'] )
+                     and ( species['species_cd'] in [18, 19, 81, 93, 94, 
+                                                     96, 351, 352, 353, 374, 
+                                                     375, 746] ))
+                                                     
+                    or (( region_id in ['S22LAZS', 'S22LCOE', 'S22LCOW',
+                                        'S22LID', 'S22LMTE', 'S22LMTW',
+                                        'S22LNMS', 'S22LUTNE', 'S22LUTSW',
+                                        'S22LWYE', 'S22LWYW'])
+                          and ( species['species_cd'] in [242, 263, 264] ))
+
+                    or ( species['species_cd'] in [741, 742, 745, 747, 748, 749] )):
+
+                # TODO implement the calculation here
+                print('Table 3 Row 4')
+
+            # Table 3 Row 5
+            elif (((region_id in ['S22LAZN', 'S22LAZS', 'S22LNMN', 'S22LNMS', 'S22LUTSW'])
+                     and ( species['species_cd'] in [51, 112, 116, 118, 122, 135, 137, 231] ))):
+                
+                # TODO implement the calculation here
+                print('Table 3 Row 5')
+
+            # Table 3 Row 6
+            elif (((region_id in ['S22LCOW', 'S22LNV', 'S22LUTNE', 'S22LWYW'])
+                     and ( species['species_cd'] in [51, 112, 116, 118, 122, 135, 137, 231] ))):
+                
+                # TODO implement the calculation here
+                print('Table 3 Row 6')
+
+            # Table 3 Row 8
+            elif (((region_id in ['S22LID', 'S22LMTE', 'S22LMTW', 'S22LNV'])
+                     and ( species['species_cd'] == 64 ))):
+                
+                # TODO implement the calculation here
+                print('Table 3 Row 8')
+
+            # Table 3 Row 11
+            elif (((region_id in ['S22LAZN', 'S22LAZS', 'S22LNMN', 'S22LNMS'] )
+                     and ( species['species_cd'] in [322, 814] ))
+
+                     or ( species['species_cd'] in [756, 757, 758, 803, 810,
+                                                    829, 843, 846, 847] )):
+                
+                # TODO implement the calculation here
+                print('Table 3 Row 11')
 
 
-    #def getGrossVolValues(self, species)
+            # Table 3 Row 12
+            elif ((region_id in ['S22LCOE', 'S22LCOW', 'S22LID', 'S22LMTE', 'S22LMTW',
+                                  'S22LNV', 'S22LUTNE', 'S22LUTSW','S22LWYE', 'S22LWYW'])
+                     and ( species['species_cd'] in [322, 814] )):
+                
+                # TODO implement the calculation here
+                print('Table 3 Row 12')
 
-    # def getVOLCFGRS(self, region, x1, x2, x3, v1, v2):
+
+            # Table 3 Row 7
+            elif species['species_cd'] in [58, 59, 62, 63, 65, 
+                                           66, 69, 106, 134, 140, 143]:
+                
+                # TODO implement the calculation here
+                print('Table 3 Row 7')
+            
+            # Table 3 Row 9
+            elif species['species_cd'] in [68, 130, 136, 313, 361, 
+                                           362, 404, 461, 462, 492, 
+                                           544, 547, 552, 602, 606, 
+                                           732, 823, 826, 901, 972, 974]:
+                
+                # TODO implement the calculation here
+                print('Table 3 Row 9')
+
+            # Table 3 Row 10
+            elif species['species_cd'] in [133, 475]:
+                
+                # TODO implement the calculation here
+                print('Table 3 Row 10')
+
+            else:
+                raise Exception("Unknown equation for region and species!")
+
+        # Pacific Northwest (AK, CA, OR, WA) Table 4
+        elif region_id in ['S26LCA', 'S26LCAMIX', 
+                           'S26LEOR', 'S26LWOR', 'S26LORJJ'
+                           'S26LEWA', 'S26LWWA', 'S26LWACF',
+                           'S27LAK', 'S27LAK1AB', 'S27LAK1C', 'S27LAK2A', 'S27LAK2B',
+                           'S27LAK2C', 'S27LAK3A', 'S27LAK3B', 'S27LAK3C', 'S27LAK3D',
+                           'S27LAK3E', 'S27LAK3F']:
 
 
-    def getVOLCFSND(self, region_id):
+            # TODO implement PNW logic here...
+            print( 'table 4 ')
+
+
+        else:
+            raise Exception("Unknown region!")
+
+
+
+    #def getVOLCFSND(self, region_id):
 
 
     # def _calcDRYBIO_BOLE(self):
